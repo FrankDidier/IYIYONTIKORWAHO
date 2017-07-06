0a9dd5fc80dead337312cfe613a0a66b73580ce8
FrankDidier <ididierfrank@yahoo.fr>
2017-07-06 11:21:15 +0800

    Update api.ai-hlpstapply.py

--- api.ai-hlpstapply.py
+++ api.ai-hlpstapply.py
@@ -2,11 +2,8 @@
 
 from __future__ import print_function
 from future.standard_library import install_aliases
-install_aliases()
 
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
+install_aliases()
 
 import json
 import os
@@ -15,7 +12,6 @@ import jsonpath
 from flask import Flask
 from flask import request
 from flask import make_response
-
 
 # Flask app should start in global layout
 app = Flask(__name__)
@@ -28,7 +24,7 @@ def webhook():
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = processRequest(req)
+    res = makeWebhookResult(req)
 
     res = json.dumps(res, indent=4)
     # print(res)
@@ -36,71 +32,49 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def processRequest(req):
-    if req.get("result").get("action") != "Bachapp":
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "Phdapp":
         return {}
-    
+    result = req.get("result")
+    parameters = result.get("parameters")
+
+    Progr = parameters.get("PhDsubjects")
+    time = parameters.get("PhdTime")
+    Levp = parameters.get("PhDDegLevp")
 
     with open('Sheet1.json') as f:
         data = f.read()
         jsondata = json.loads(data)
-    # data = json.loads(result)
-    res = makeWebhookResult(jsondata)
-    return res
 
-def makejsonQuery(req):
-    result = req.get("result")
-    parameters = result.get("parameters")
-    Levp = parameters.get("BachLevp")
-    Progr = parameters.get("BachSubject")
-    time = parameters.get("BachTime")
-    if Levp is None:
-        return None
-    elif Progr is None:
-        return None
-    elif time is None:
-        return None
+    #jsondata =
 
-    match = jsonpath.jsonpath(jsondata,'$.features[[?(@.ProgramName == Progr && @.Level == Levp && @.StartDate == time)]].UniversityName,Program URL,Years,App Deadline,1stYrTuition,Tuition')
-    return match
+    match = jsonpath.jsonpath(jsondata,
+                              '$.features[[?(@.ProgramName == Progr && @.Level == Levp && @.StartDate == time)]].UniversityName,Program URL,Years,App Deadline,1stYrTuition,Tuition')
+    # return match
 
-
-def makeWebhookResult(data):
-    query = data.get('query')
-    if query is None:
-        return {}
-
-    result = query.get('results')
-    if result is None:
-        return {}
-
-    channel = result.get('channel')
-    if channel is None:
-        return {}
-
-    Univname = channel.get('UniversityName')
-    progname = channel.get('ProgramName')
-    progurl = channel.get('Program URL')
-    yaz = channel.get('Years')
-    Appdead = channel.get('App Deadline')
-    FirstTuit = channel.get('1stYrTuition')
-    Tuit = channel.get('Tuition')
-
-    if (progname is None) or (progurl is None) or (yaz is None) or (Appdead is None) or (FirstTuit is None) or (Univname is None) or (Tuit is None):
-        return {}
-    
-    speech = "Hello Bro this is webhook"
-
-    #speech = "Would you like to study in " + Univname + ": " + progname + \
+    # speech = "Would you like to study in " + Univname + ": " + progname + \
     #         ", click this link to apply to this program " + progurl + "With Application Deadline on " + Appdead + \
     #         ",Whereby First Year Tuition fees is " + FirstTuit + "In total Tuition Fees is " + Tuit + \
     #         ",this program will take " + yaz + "Years"
-
     
-    #speech = "Would you like to study in " + Univname.get('UniversityName') + ": " + progname.get('ProgramName') + \
-    #        ", click this link to apply to this program " + progurl.get('Program URL') + "With Application Deadline on " + Appdead.get('Tuition') + \
-    #         ",Whereby First Year Tuition fees is " + FirstTuit.get('1stYrTuition') + "In total Tuition Fees is " + Tuit.get('Tuition') + \
-    #         ",this program will take " + yaz.get('Years') + "Years"
+    query = req.get('query')
+    if query is None:
+        return {}
+    
+    result = query.get('results')
+    if result is None:
+        return {}
+    
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+    
+    Univname = channel.get('PhDsubjects')
+        
+
+
+    speech = Univname
 
     print("Response:")
     print(speech)
@@ -109,9 +83,10 @@ def makeWebhookResult(data):
         "speech": speech,
         "displayText": speech,
         # "data": data,
-        # "contextOut": [],
+        # "contextOut": [[{"name":"phd", "lifespan":5}],
         "source": "marcopolo1995"
     }
+
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
