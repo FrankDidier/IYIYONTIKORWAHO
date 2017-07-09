27455c5621045583fba26ba5ac448bcf8e070b1e
FrankDidier <ididierfrank@yahoo.fr>
2017-07-10 06:43:16 +0800

    Update api.ai-hlpstapply.py

--- api.ai-hlpstapply.py
+++ api.ai-hlpstapply.py
@@ -32,42 +32,15 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def getlev(reql):
-    if reql.get("result").get("action") != "Phdapp":
-        return {}
-    result1 = reql.get("result")
-    parameters = result1.get("parameters")
-    Levp = parameters.get("PhDDegLevp")
-    
-    return Levp
-
-def getpro(reqp):
-    if reqp.get("result").get("action") != "Phdapp":
-        return {}
-    result2 = reqp.get("result")
-    parameters = result2.get("parameters")
-    Progr = parameters.get("PhDsubjects")
-    
-    return Progr
-
-def gettime(reqt):
-    if reqt.get("result").get("action") != "Phdapp":
-        return {}
-    result3 = reqt.get("result")
-    parameters = result3.get("parameters")
-    time = parameters.get("PhdTime")
-    
-    return time
-
 def makeWebhookResult(req):
     if req.get("result").get("action") != "Phdapp":
         return {}
-    #result = req.get("result")
-    #parameters = result.get("parameters")
+    result = req.get("result")
+    parameters = result.get("parameters")
 
-    #Progr = parameters.get("PhDsubjects")
-    #time = parameters.get("PhdTime")
-    #Levp = parameters.get("PhDDegLevp")
+    Progr = parameters.get("PhDsubjects")
+    time = parameters.get("PhdTime")
+    Levp = parameters.get("PhDDegLevp")
     
     
     #Levp = str(input("What level do you want to study:\n"))
@@ -82,7 +55,7 @@ def makeWebhookResult(req):
 
 
     match_list = jsonpath.jsonpath(jsondata,
-                              '$.features[[?(@.ProgramName == getpro(reqp) && @.Level == getlev(reql) && @.StartDate == gettime(reqt))]].UniversityName,Program URL,App Deadline,1stYrTuition')
+                              '$.features[[?(@.ProgramName == 'Progr' && @.Level == 'Levp' && @.StartDate == 'time')]].UniversityName,Program URL,App Deadline,1stYrTuition')
     match_str = ", #".join(match_list)
 
 
