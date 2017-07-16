6f266823e0f37c3f0c45b5aa878f22c41d99c042
FrankDidier <ididierfrank@yahoo.fr>
2017-07-16 18:10:27 +0800

    Update api.ai-hlpstapply.py

--- api.ai-hlpstapply.py
+++ api.ai-hlpstapply.py
@@ -53,10 +53,10 @@ def makeWebhookResult(req):
 
     match_list = jsonpath.jsonpath(jsondata,
                               '$.features[[?(@.ProgramName == "{0}".format(Progr) && @.Level == "{0}".format(Levp) && @.StartDate == "{0}".format(time)))]].UniversityName,Program URL,App Deadline,1stYrTuition')
-    match_str = ", #".join(match_list)
+    #match_str = ", #".join(match_list)
 
 
-    speech = "These are universities you were looking for :) with their Program direct-link ,Application Deadline with first year Tuition Fees:=>" + match_str
+    speech = "These are universities you were looking for :) with their Program direct-link ,Application Deadline with first year Tuition Fees:=>" + match_list
 
     print("Response:")
     print(speech)
