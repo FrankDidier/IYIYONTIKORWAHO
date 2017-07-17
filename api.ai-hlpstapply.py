fd336f938394874c5ebf3212ff58caaa80bcc819
FrankDidier <ididierfrank@yahoo.fr>
2017-07-18 03:12:39 +0800

    Update api.ai-hlpstapply.py

--- api.ai-hlpstapply.py
+++ api.ai-hlpstapply.py
@@ -67,7 +67,7 @@ def makeWebhookResult(req):
     #match_str = ", #".join(match_list)
 #
 
-    speech = "These are universities you were looking for :) with their Program direct-link ,Application Deadline with first year Tuition Fees:=>" + str(match_list)+ Progr
+    speech = "These are universities you were looking for :) with their Program direct-link ,Application Deadline with first year Tuition Fees:=>" + str(match_list)+ str(Progr)
 
     print("Response:")
     print(speech)
