b8b0eeab5e32eaf7a3c70d068f8051c88a3f9ca8
FrankDidier <ididierfrank@yahoo.fr>
2017-07-18 23:57:42 +0800

    Update api.ai-hlpstapply.py

--- api.ai-hlpstapply.py
+++ api.ai-hlpstapply.py
@@ -4,7 +4,8 @@ from __future__ import print_function
 from future.standard_library import install_aliases
 
 install_aliases()
-
+import codecs
+import csv
 import json
 import os
 import jsonpath
@@ -34,55 +35,85 @@ def webhook():
     r.headers['Content-Type'] = 'application/json'
     return r
 #
-def trial(req):
-    if req.get("result").get("action") == "Phdapp":
-        result = req.get("result")
-        parameters = result.get("parameters")
 
-        Progr = parameters.get("PhDsubjects")
-        Pr=''.join(Progr)
-        
-        return Pr
 def makeWebhookResult(req):
     if req.get("result").get("action") == "Phdapp":
         result = req.get("result")
         parameters = result.get("parameters")
 
-        #Progr = parameters.get("PhDsubjects")
+        Progr = parameters.get("PhDsubjects")
     
         tme = parameters.get("PhdTime")
     
         Levp = parameters.get("PhDDegLevp")
         
-        #Pr=''.join(Progr)
-        #A = json.loads(Pr)
-        #yes=json.dumps(Pr)
-        #A=str(Pr)
-        #Ti=''.join(tme)
-        #qew=json.dumps(Ti)
-        #B=str(Ti)
-        #Le=''.join(Levp)
-        #vbe=json.dumps(Le)
-        #C=str(Le)
+        Pr=''.join(Progr)
+        Ti=''.join(tme)
+        Le=''.join(Levp)
+       
         
+    query = 'Program_Name == Pr and Level == Le and Start_Date == Ti'
+    csvData = csv.reader(codecs.open('test.csv','rb',encoding='latin-1'))
+    csvTable = []
+    isHeader = True
+    for row in csvData:
+        if isHeader = False
+        headerRow = row
+        for i in range(len(headerRow)):
+            headerRow[i] = headerRow[i].replace(' ', '_')
+        else:
+            csvTable.append(row)
+    colType = []
+    for i in range(len(headerRow)):
+        isFloat = True
+        isInt = True
+        for j in range(len(csvTable)):
+            try:
+                v = float(csvTable[j][i])
+                if not v == int(v):
+                    isInt = False
+            except ValueError:
+                isFloat = False
+                isInt = False
+                 
+        colT = ''
+        if isInt:
+            colT = 'int'
+        elif isFloat:
+            colT = 'float'
+        else:
+            colT = 'string'
+        colType.append(colT)
+        
+    for j in range(len(csvTable)):
+        for i in range(len(headerRow)):
+            if colType[i] == 'string':
+                exec(headerRow[i] + '=' + '"' + csvTable[j][i] + '"')
+            elif colType[i] == 'float':
+                exec(headerRow[i] + '=' + 'float("' + csvTable[j][i] + '")')
+            elif colType[i] == 'int':
+                exec(headerRow[i] + '=' + 'int("' + csvTable[j][i] + '")')
+        t = csvTable[j]
+                    
     
 
-    with open('Sheet1.json') as f:
-        data = f.read()
-        jsondata = json.loads(data)
+    #with open('Sheet1.json') as f:
+    #    data = f.read()
+    #    jsondata = json.loads(data)
     #using Jsonpath_rw
     #match_list = jsonpath_rw.parse("$.features[[?(@.ProgramName == A && @.Level == C  && @.StartDate == B )]].UniversityName,Program URL,App Deadline,1stYrTuition").find(jsondata)
     #y="bachelor's"
     #Using jsonpath
-    t="{}".format(trial(req))
-    match_list = jsonpath.jsonpath(jsondata,
-                              '$.features[[?(@.ProgramName == t && @.Level == "PhD"  && @.StartDate == "September" )]].UniversityName,Program URL,App Deadline,1stYrTuition')
+    #t="{}".format(trial(req))
+    #match_list = jsonpath.jsonpath(jsondata,
+    #                          '$.features[[?(@.ProgramName == t && @.Level == "PhD"  && @.StartDate == "September" )]].UniversityName,Program URL,App Deadline,1stYrTuition')
     
     
     #match_str = ", #".join(match_list)
 #
 
-    speech = "These are universities you were looking for :) with their Program direct-link ,Application Deadline with first year Tuition Fees:=>" + str(match_list) + t
+    speech = "These are universities you were looking for :) with their Program direct-link ,Application Deadline with first year Tuition Fees:=>" + str(t)
+    #str(match_list) + t
     #+ json.dumps(Progr) + yes
 
     print("Response:")
