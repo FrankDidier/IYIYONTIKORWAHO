334b9546cf6b8ae8f9a43dbf88c06a0d63704dd9
FrankDidier <ididierfrank@yahoo.fr>
2017-07-19 11:15:17 +0800

    Update api.ai-hlpstapply.py

--- api.ai-hlpstapply.py
+++ api.ai-hlpstapply.py
@@ -53,7 +53,7 @@ def makeWebhookResult(req):
 
     query = 'Program_Name == Pr and Level == Le and Start_Date == Ti'
     #query = 'id > 0 and code_name == "def" and value <=100'
-    csvData = csv.reader(codecs.open('bestsheet.csv','rU',encoding='latin-1'))
+    csvData = csv.reader(codecs.open('bestsheet.csv','rU',encoding='utf-16'))
     csvTable = []
     isHeader = True
     for row in csvData:
