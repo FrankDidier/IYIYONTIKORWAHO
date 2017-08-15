9c433657f39d47bbdf85a6a81f69bdb882e3460b
FrankDidier <ididierfrank@yahoo.fr>
2017-08-16 00:51:53 +0800

    Update api.ai-hlpstapply.py

--- api.ai-hlpstapply.py
+++ api.ai-hlpstapply.py
@@ -54,7 +54,7 @@ def makeWebhookResult(req):
         server.login("testapiblcu@gmail.com", "Testapi2017")
 
         msg = "A user need further help at china admission -> MARCO POLO Bot -> his/her E-mail & his/her Country"+str(Addr) +str(Countr)
-        server.sendmail("testapiblcu@gmail.com", "ididierfrank@yahoo.fr", msg)
+        server.sendmail("testapiblcu@gmail.com", "richcoward@gmail.com", msg)
         server.quit()
         #speech = "Thank you for Providing requested info ,One of our advisors will contact you. You can also call us on: +86 132 4122 2181"
         
